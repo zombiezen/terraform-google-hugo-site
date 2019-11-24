ebda9a66699d81267f9ac3da47ff29f0ebabf9d2
Ross Light <ross@zombiezen.com>
2019-11-24 09:26:25 -0800

    Upgrade to latest Hugo version

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ data "local_file" "hugo_dockerfile" {
 }
 
 locals {
-  hugo_image     = "gcr.io/${data.google_project.project.project_id}/hugo:0.53"
+  hugo_image     = "gcr.io/${data.google_project.project.project_id}/hugo:0.59.1"
   firebase_image = "gcr.io/${data.google_project.project.project_id}/firebase"
 }
 
